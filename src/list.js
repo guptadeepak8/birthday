d4afe1ed45f3c3b41416fef70cf5a689bd05d2bf
guptadeepak8 <vectord7738@gmail.com>
2022-08-13 19:30:33 +0530

    learned hooks

---
+++ src/list.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const List=({people})=>{
+  return(
+  <>
+   {people.map((person)=>{
+    const {id,image,name,age}=person
+    return(
+      <article key={id} className="people">
+        <img src={image} alt="" />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+        </div>
+      </article>
+    )
+   })}
+  </>
+  )
+}
+export default List
